fdf70ac414fb3ac478269d4c19737199c1582d91
Matteo Bertozzo <matteobertozzo@outlook.com>
2018-10-08 17:57:15 +0200

    Some more complexity

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ import { html, render } from './node_modules/lit-html/lit-html.js';
 
 const root = document.querySelector('#root');
 
-//.toLocaleDateString restituisce la data nel formato impostato per il browser
-const date = () => new Date().toString();
-const template = value => html`<h1>Oggi è il <span>${value}</span></h1>`;
+const avatar = url => html`
+  <figure>
+    <img src="${url}"
+  </figure>
+`;
 
-setInterval(() => render( template(date()), root ), 1000);
+const details = (username, email) => html`
+  <p>
+    Your username is <strong>${username}</strong> and your email is <strong>${email}</strong>.
+  </p>
+`;
+
+const template = data => html`
+  <article>
+    ${avatar(data.picture.thumbnail)}
+    <p>Hello <span style="text-transform: capitalize;">${data.name.first} ${data.name.last}!</span></p>
+    ${details(data.login.username, data.email)}
+    </article>
+  `;
+
+fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  //.then(response => console.log(JSON.stringify(response)));
+  .then(response => response.results[0])
+  .then(response => render(template(response),root));
